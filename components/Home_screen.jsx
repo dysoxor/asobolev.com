b0d14f57d2f4f823e9d86ace88645aadd254e280
dysoxor <asobolev.pro@gmail.com>
2022-11-11 05:54:09 -0500

    position inherit

--- components/Home_screen.jsx
+++ components/Home_screen.jsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 
 
 import pdp from '../assets/pdp_carre.png'
-import logo_web from '../assets/logo_web.png'
 
 import Link from 'next/link'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -30,7 +29,7 @@ const Home_screen = () => {
             <div className='typing-home-hello-world'><FontAwesomeIcon icon={faChevronRight} style={{fontSize: '74px', fontWeight:'100' }}/> &quot;hello world&quot;</div>
           </div>
           <div className='home-name'>
-            <div className='typing-home-name'><FontAwesomeIcon icon={faChevronRight} style={{fontSize: '74px', fontWeight:'100' }} /> <span className='home-logo'>I&apos;m <Image src={logo_web} alt='website logo' width={70} height={70} style={{marginBottom:'-0.1em',marginRight:'-0.08em'}}/>ndrey</span></div>
+            <div className='typing-home-name'><FontAwesomeIcon icon={faChevronRight} style={{fontSize: '74px', fontWeight:'100' }} /> <span className='home-logo'>I&apos;m <span className='home-left-logo'>A</span><span className='home-right-logo'>A</span>ndrey</span></div>
           </div>
           <div className='home-title typing-home-title'>
             <div className="typing-home-title"> <ul className='titles-home-container'> <li style={{display: 'inline'}}><FontAwesomeIcon icon={faChevronRight} style={{fontSize: '74px', fontWeight:'100' }}/> </li> <li className='software-engineer-home'><span>Software Engineer</span></li> <li className='data-scientist-home'><span>Data Scientist</span></li> <li className='full-stack-home'><span>Full Stack Developer</span></li> </ul> </div>
